3c3288db3a7714f360840b564928a7b9e288045b
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-01-22 17:54:22 +0000

    mcmc step completed possibly?

---
+++ HINTS.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy.stats import multivariate_normal
+from tqdm import tqdm
+import scipy.stats
+
+class HINTS():
+
+    def __init__(self,mu,sigma):
+        self.mu=mu
+        self.sigma=sigma
+
+
+    def mcmc(self, x):                           # q is proposal dist and f is posterior dist (How to incorporate this into sampler, or just leave it as gaussian for now?)
+        dim=len(x)                       # dimension of sample
+        xn = np.zeros((dim))                           # set up vector for new sample
+        xn = multivariate_normal.rvs(x, np.eye(dim))   # take new sample
+
+        a = multivariate_normal.logpdf(xn, self.mu, self.sigma)     # define mu and sigma in init since they wont change for the entire thing? 
+        b = multivariate_normal.logpdf(x, self.mu, self.sigma)      
+        alpha = a/b
+        u = np.random.uniform(0,1,1)
+        if u <= alpha:
+            return xn
+        else:
+            return x
