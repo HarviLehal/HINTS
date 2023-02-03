21007012fbab87012298f713c20b5d26de18cbf7
HarviLehal <h.lehal@liverpool.ac.uk>
2023-02-03 15:33:36 +0000

    mcmc works

--- HINTS.py
+++ HINTS.py
@@ -7,25 +7,34 @@ from tqdm import tqdm
 
 class HINTS():
 
-    def __init__(self, mu, sigma):
+    def __init__(self, mu, sigma, step):
         self.mu = mu
         self.sigma = sigma
+        self.dim = len(sigma)
+        self.step = step
 
-    def mcmc(self, x):  # q is proposal dist and f is posterior dist (How to incorporate this into sampler, or just leave it as gaussian for now?)
-        dim = len(x)      # dimension of sample
-        xn = np.zeros((dim))                           # set up vector for new sample
-        xn = multivariate_normal.rvs(x, np.eye(dim))   # take new sample
-
-        a = multivariate_normal.logpdf(xn, self.mu, self.sigma)     # define mu and sigma in init since they wont change for the entire thing?
-        b = multivariate_normal.logpdf(x, self.mu, self.sigma)
-        alpha = a/b
-        u = np.random.uniform(0, 1, 1)
-        if u <= alpha:
-            return xn
+    def mcmc(self, x, sigma, sigma_n=None):  # q is proposal dist and f is posterior dist (How to incorporate this into sampler, or just leave it as gaussian for now?)
+        mu = np.mean(x)
+        if sigma_n is not None:
+            pass
         else:
-            return x    # do I want to return a new value or new parameter estimates? or am I confusing theta for a parameter when it is infact the data?
+            sigma_n = multivariate_normal.rvs(sigma, np.eye(self.dim)*self.step**2)
+
+        prec_n = sigma_n**(-1)                          # Precision of proposal
+        prec = sigma*(-1)                               # Precision of prior
+        f = np.vstack(x-mu)
+        a = (0.5 * prec*f.T@f)-(0.5 * prec_n*f.T@f)     # Acceptance Ratio
+        u = np.random.uniform(0, 1, 1)
+        if u <= a:
+            print("Accept: "+ str(sigma_n))
+            return sigma_n
+        else:
+            print("Reject: "+ str(sigma_n))
+            return sigma
 
 
-z = HINTS([1], [0.5])
-
-z.mcmc([1.25])
+sigma = np.array([0.5])
+x = np.array([1, 2, 3, 4, 5, 6])
+mu = np.array([4])
+z = HINTS(mu, sigma, 2)
+z.mcmc(x, sigma)
