52a249dfa8aa5aeeeee67e24137d4ca2c64df7a0
HarviLehal <h.lehal@liverpool.ac.uk>
2023-03-06 11:51:08 +0000

    first attempt at first level of HINTS

--- HINTS.py
+++ HINTS.py
@@ -5,16 +5,29 @@ from Tree import Tree
 from MCMC import MCMC
 from Target import *
 from Proposal import Proposal
+from sklearn.datasets import make_spd_matrix
+from tqdm import tqdm
+
 # TEST DATA SET
 
 mu = np.array([3, 5, 7])
-sigma = np.eye(3)
+sigma = np.eye(3)*0.25
 
-x = multivariate_normal.rvs(mu, sigma, 1024)
+x = multivariate_normal.rvs(mu, sigma, 1024000)
 
 mu0 = np.array([2, 4, 6])
-sigma0 = np.eye(3)*2
+sigma0 = np.eye(3)
 theta0 = {0: mu0, 1: sigma0}
+
+# WHACKY TEST DATA SET
+
+# mu = np.array([3, 5, 7, 9])                                                                 # Sampling Mean
+# sigma = make_spd_matrix(n_dim=4) * 10
+# x = multivariate_normal.rvs(mu, sigma, 10240)
+# mu0 = np.array([4, 8, 12, 16])
+# sigma0 = make_spd_matrix(n_dim=4) * 10
+# theta0 = {0: mu0, 1: sigma0}
+
 
 # TEST TREE
 
@@ -42,12 +55,12 @@ theta0 = {0: mu0, 1: sigma0}
 
 class HINTS():
 
-    def __init__(self, x, levels, log_branch_factor, theta0, target, proposal, M):
+    def __init__(self, x, levels, log_branch_factor, theta0, target, proposal, M, stepsize):
         tree = Tree(x, levels, log_branch_factor)   # Tree Object
         tree.build_tree()                           # Build Tree
         self.target = target                        # Target Distribution
         tree.leaf_nodes()                           # Leaf Nodes
-        self.data = tree.data                       # Data tree structure
+        self.tree_structure = tree.data                       # Data tree structure
         self.leaves = tree.leaves                   # Data tree leaf nodes
         self.theta0 = theta0                        # Initial Parameter values
         self.logpdf = target.logpdf                 # Logpdf of Target
@@ -58,6 +71,23 @@ class HINTS():
         self.log_branch_factor = log_branch_factor  # Log Branch Factor
         self.design = tree.design                   # Design Matrix
         self.scenarios = tree.scenarios             # Number of Scenarios
+        self.stepsize = stepsize                    # Stepsize
+
+    def prop(self, theta):                  # Theta previous parameter, Theta_n proposal parameter
+        theta_n = self.proposal(theta, self.stepsize)
+        return theta_n
+
+    def ratio(self, x, theta, theta_n):
+
+        a = self.logpdf(x, *theta_n)        # logpdf of proposal
+        b = self.logpdf(x, *theta)          # logpdf of previous
+        a = a-b                             # Acceptance Ratio
+        a = np.exp(a)                       # Exponent
+        u = np.random.uniform(0, 1, 1)
+        if u <= a:
+            return theta_n                  # Accept Proposal
+        else:
+            return theta                    # Reject Proposal
 
     def init_leaf_selection(self):
         init_leaf = random.choice(self.leaves)
@@ -76,18 +106,37 @@ class HINTS():
         node_ids = self.init_leaf_set_node_ids()
         leaf_data = []
         for node_id in sorted(node_ids):
-            node = self.data[node_id]
+            node = self.tree_structure[node_id]
             if node.level == self.levels:
                 leaf_data.append(node.data)
         return leaf_data
 
+    def sampler(self):
+        thetas = []
+        thetas.append(self.theta0)
+        init_leaf_set = self.init_leaf_set_node_ids()
+        for i in tqdm(range(self.M-1)):
+            for id in init_leaf_set:
+                thetan = {}
+                for j in range(len(self.theta0)):
+                    thetan[j] = self.prop(thetas[i][j])
+                thetan = self.ratio(self.tree_structure[id].data, list(thetas[i].values()), list(thetan.values()))
+                p = {}
+                for j in range(len(self.theta0)):
+                    p[j] = thetan[j]
+                thetas.append(p)
+        self.thetas = thetas
+        self.plot(self.thetas)
 
-z = HINTS(x, 4, 2, theta0, Gaussian, Proposal.rw3, 1000)
+
+z = HINTS(x, 4, 2, theta0, Gaussian, Proposal.rw3, 10000, 0.1)
 a = z.init_leaf_selection()
 b = z.init_leaf_set_node_ids()
 c = z.init_leaf_set_data()
 
-for i in a:
-    print(i.__dict__)
-print(b)
-print(c)
+# for i in a:
+#     print(i.__dict__)
+# print(b)
+# print(c)
+
+z.sampler()
