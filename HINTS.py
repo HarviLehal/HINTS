236b5e4505c50573b92638931f596452ec921994
HarviLehal <h.lehal@liverpool.ac.uk>
2023-03-07 18:31:27 +0000

    HINTS is slow!!!

--- HINTS.py
+++ HINTS.py
@@ -11,12 +11,12 @@ from tqdm import tqdm
 # TEST DATA SET
 
 mu = np.array([2, 4, 6])
-sigma = np.eye(3)*0.25
+sigma = np.eye(3)
 
-x = multivariate_normal.rvs(mu, sigma, 1024000)
+x = multivariate_normal.rvs(mu, sigma, 1024)
 
 mu0 = np.array([8, 16, 22])
-sigma0 = np.eye(3)
+sigma0 = np.eye(3)*2
 theta0 = {0: mu0, 1: sigma0}
 
 # WHACKY TEST DATA SET
@@ -65,20 +65,31 @@ class HINTS():
         else:
             return theta                    # Reject Proposal
 
-    def init_leaf_selection(self):
-        init_leaf = random.choice(self.leaves)      # Randomly select initial leaf node
-        init_leaf_set = []                          # Initialise set of leaf nodes
-        for x in self.leaves:                       # Select all leaf nodes with same parent
-            if x.parent_id == init_leaf.parent_id:
-                init_leaf_set.append(x)
-        return init_leaf_set
+    def rand_leaf_selection(self):              # Random Leaf Selection
+        rand_leaf = random.choice(self.leaves)
+        return rand_leaf
 
-    def init_leaf_set_node_ids(self):               # Initialise leaf set node ids
-        init_leaf_set = self.init_leaf_selection()
-        node_ids = [node.node_id for node in init_leaf_set]
-        return sorted(node_ids)
+    def level_set(self, level):                 # Level Set
+        level_set = []
+        for x in self.tree_structure:
+            if x.level == level:
+                level_set.append(x)
+        return level_set
 
-    def init_leaf_set(self, node_ids):              # Initialise leaf set
+    def common_parent(self, node):        # Common Parent Node Set
+        common_parent_set = []
+        level_set = self.level_set(node.level)  # Level Set
+        for x in level_set:           # ADD SOMETHING TO FACTOR LEVELS IN
+            if x.parent_id == node.parent_id:
+                common_parent_set.append(x)
+
+        return common_parent_set
+
+    # def common_parent_node_ids(self, common_parent_set):            # Common Parent Node Ids
+    #     node_ids = [node.node_id for node in common_parent_set]
+    #     return sorted(node_ids)
+
+    def init_leaf_set(self, node_ids):
         leaf_data = []
         for node_id in sorted(node_ids):
             node = self.tree_structure[node_id]
@@ -86,59 +97,54 @@ class HINTS():
                 leaf_data.append(node)
         return leaf_data
 
+    def parent(self, node):
+        parentnode = []
+        level_set = self.level_set(node.level - 1)  # Level Set
+        for x in level_set:           # ADD SOMETHING TO FACTOR LEVELS IN
+            if x.node_id == node.parent_id:
+                return x
+
     def sampler(self):
-        thetas = []                                             # Initialise theta bottom level list
-        thetas2 = []                                            # Initialise theta 2nd lowest level list
-        thetas.append(self.theta0)                              # Append initial theta
-        init_leaf_ID = self.init_leaf_set_node_ids()            # Initialise leaf set node ids
-        init_leaf_sets = self.init_leaf_set(init_leaf_ID)       # Initialise leaf set
-        for i in tqdm(range(self.M-1)):                         # HINTS Iterations
-            for id in init_leaf_ID:                             # Iterate through leaf set
-                thetan = {}
-                for j in range(len(self.theta0)):               # Iterate through parameters
-                    thetan[j] = self.prop(thetas[i][j])         # Propose new parameter
-                thetan = self.ratio(self.tree_structure[id].data, list(thetas[i].values()), list(thetan.values()))  # Acceptance Ratio
+        thetas = [[] for i in range(self.levels+1)]               # Initialise theta level list
+        total = []
+        total.append(self.theta0)
+        thetas[self.levels].append(self.theta0)                           # Append initial theta
+        for iter in tqdm(range(self.M-1)):                      # HINTS Iterations
+            init_leaf = self.rand_leaf_selection()                # Random Leaf Selection
+            parent = self.parent(init_leaf)
+            for level in reversed(range(1, self.levels+1)):                    # Iterate through levels   (DONT FORGET TO DO self.levels - level so it starts at the bottom))
+                if level == self.levels:                # If level is lowest
+                    common_parent_set = self.common_parent(init_leaf)  # Common Parent Set
+                else:
+                    common_parent_set = self.common_parent(parent)  # Common Parent Set
+                for index, node in enumerate(common_parent_set):                      # Iterate through common parent set
+                    thetan = {}
+                    for j in range(len(self.theta0)):               # Iterate through parameters
+                        thetan[j] = self.prop(thetas[level][iter+index][j])  # Propose new parameter
+                    thetan = self.ratio(node.data, list(thetas[level][iter+index].values()), list(thetan.values()))  # Acceptance Ratio
+                    p = {}
+                    for j in range(len(self.theta0)):
+                        p[j] = thetan[j]
+                    thetas[level].append(p)                         # Append new theta
+                    total.append(p)
+                parent = self.parent(common_parent_set[0])                     # Parent Node
+                thetan = self.ratio(parent.data, list(thetas[level][iter].values()), list(thetas[level][iter+len(common_parent_set)].values()))  # Acceptance Ratio
                 p = {}
                 for j in range(len(self.theta0)):
                     p[j] = thetan[j]
-                thetas.append(p)                                # Append new theta
-
-            for x in self.tree_structure:
-                if init_leaf_sets[0].parent_id == x.node_id:    # Select parent node of leaf set
-                    parent = x
-            thetan = {}
-            for j in range(len(self.theta0)):                   # Iterate through parameters
-                thetan[j] = thetas[i-len(init_leaf_ID)][j]      # Propose new parameter
-            thetan = self.ratio(parent.data, list(thetas[i].values()), list(thetan.values()))   # Acceptance Ratio
-            p = {}
-            for j in range(len(self.theta0)):                   # Append new theta to theta2
-                p[j] = thetan[j]
-            thetas2.append(p)
+                thetas[level-1].append(p)
+                total.append(p)
         self.thetas = thetas
-        self.thetas2 = thetas2
-        self.plot(self.thetas)
-        self.plot(self.thetas2)
-
-    def sampler2(self):
-        thetas = [[] for i in range(self.levels)]               # Initialise theta level list
-        thetas[0].append(self.theta0)                           # Append initial theta
-        for iter in tqdm(range(self.M-1)):                      # HINTS Iterations
-            init_leaf_ID = self.init_leaf_set_node_ids()        # Initialise leaf set node ids
-            for x in self.tree_structure:
-                if init_leaf_sets[0].parent_id == x.node_id:    # Select parent node of leaf set
-                    parent = x
-            for levels in range(self.levels):
-
-            init_leaf_sets = self.init_leaf_set(init_leaf_ID)   # Initialise leaf set
-            for id in init_leaf_ID:                             # Iterate through leaf set
-                thetan = {}
-                for j in range(len(self.theta0)):               # Iterate through parameters
-                    thetan[j] = self.prop(thetas[0][iter][j])   # Propose new parameter
-                thetan = self.ratio(self.tree_structure[id].data, list(thetas[0][iter].values()), list(thetan.values()))  # Acceptance Ratio
+        self.total = total
+        self.plot(self.total)
+        # for i in range(len(self.thetas)):
+        #     self.plot(self.thetas[i])
 
 
-
-z = HINTS(x, 4, 2, theta0, Gaussian, Proposal.rw3, 10000, 0.1)
+z = HINTS(x, 4, 2, theta0, Gaussian, Proposal.rw3, 1000, 0.1)
 
 z.sampler()
 
+
+z2 = MCMC(x, theta0, Gaussian, Proposal.rw3, 0.1)
+z2.mcmc(1000)
--- tree.py
+++ tree.py
@@ -1,13 +1,73 @@
 import numpy as np
 
 
+# class Node:
+
+#     def __init__(self, node_id, data, level=None):
+#         self.node_id = node_id      # Node ID
+#         self.data = data            # Data
+#         self.level = level          # Level
+
+
+# class Tree:
+#     def __init__(self, x, levels, log_branch_factor):
+#         self.levels = levels                                                                # Number of Levels
+#         self.log_branch_factor = log_branch_factor                                          # Log Branch Factor
+#         self.design = np.array([1] + [2 ** self.log_branch_factor for _ in range(self.levels)])     # Design Matrix
+#         self.scenarios = 1 * 2 ** (self.levels * self.log_branch_factor)                    # Number of Scenarios
+#         self.x = np.split(x, self.scenarios)                                                # Split Data into Scenarios
+
+#     def build_tree(self):
+#         D = []
+#         node = 0
+#         for i in range(self.levels):
+#             if i == 0:
+#                 D.append([])
+#                 for j in range(self.scenarios):
+#                     node_id = node + j
+#                     data = self.x[j]
+#                     D[i].append(Node(node_id, data, self.levels - i))
+#                 z = int(self.scenarios/self.design[-1])
+#                 for j in range(self.scenarios):
+#                     D[i][j].parent_id = self.scenarios + j % z
+#                     D[i][j].level = self.levels - i
+
+#             else:
+#                 D.append([])
+#                 for j in range(self.levels**(self.levels-i)):
+#                     node_id = node + j
+#                     data = self.x[j]
+#                     D[i].append(Node(node_id, data, self.levels - i))
+
+#                 z = int(self.levels**(self.levels-i-1))
+#                 for j in range(self.levels**(self.levels-i)):
+#                     D[i][j].parent_id = node + self.levels**(self.levels-i) + j % z
+#                     D[i][j].level = self.levels - i
+#             node += self.levels**(self.levels-i)
+#         D.append([])
+#         node_id = node
+#         data = self.x
+#         level = 0
+#         D[-1].append(Node(node_id, data, level))
+#         D = [elem for sublist in D for elem in sublist]
+#         self.data = D
+
+#     def leaf_nodes(self):
+#         leaves = [] # List of leaf nodes
+#         for x in self.data: # Iterate through nodes
+#             if x.level == self.levels:  # If node is a leaf node
+#                 leaves.append(x)    # Add leaf node to list
+#         self.leaves = leaves    # Leaf nodes
+
+
+
 class Node:
 
-    def __init__(self, node_id, data, level=None):
+    def __init__(self, node_id, data, level=None, parent_id=None):
         self.node_id = node_id      # Node ID
         self.data = data            # Data
         self.level = level          # Level
-
+        self.parent_id = parent_id       # Parent ID
 
 class Tree:
     def __init__(self, x, levels, log_branch_factor):
@@ -23,26 +83,23 @@ class Tree:
         for i in range(self.levels):
             if i == 0:
                 D.append([])
+                z = int(self.scenarios/self.design[-1])
                 for j in range(self.scenarios):
                     node_id = node + j
                     data = self.x[j]
-                    D[i].append(Node(node_id, data, self.levels - i))
-                z = int(self.scenarios/self.design[-1])
-                for j in range(self.scenarios):
-                    D[i][j].parent_id = self.scenarios + j % z
-                    D[i][j].level = self.levels - i
+                    parent_id = self.scenarios + j % z
+                    level = self.levels - i
+                    D[i].append(Node(node_id, data, level, parent_id))
 
             else:
                 D.append([])
+                z = int(self.levels**(self.levels-i-1))
                 for j in range(self.levels**(self.levels-i)):
                     node_id = node + j
                     data = self.x[j]
-                    D[i].append(Node(node_id, data, self.levels - i))
-
-                z = int(self.levels**(self.levels-i-1))
-                for j in range(self.levels**(self.levels-i)):
-                    D[i][j].parent_id = node + self.levels**(self.levels-i) + j % z
-                    D[i][j].level = self.levels - i
+                    parent_id = node + self.levels**(self.levels-i) + j % z
+                    level = self.levels - i
+                    D[i].append(Node(node_id, data, level, parent_id))
             node += self.levels**(self.levels-i)
         D.append([])
         node_id = node
@@ -58,46 +115,3 @@ class Tree:
             if x.level == self.levels:  # If node is a leaf node
                 leaves.append(x)    # Add leaf node to list
         self.leaves = leaves    # Leaf nodes
-
-    # def build_tree(self):
-    #     D = []
-    #     node = 0
-    #     for i in range(self.levels):    # Iterate through levels
-    #         if i == 0:  # If first level
-    #             D.append([])
-    #             for j in range(self.scenarios):                         # Add child nodes to parent node
-    #                 node_id = node + j                                  # Node ID
-    #                 data = self.x[j]                                    # Data
-    #                 D[i].append(Node(node_id, data, self.levels - i))   # Add node to list
-    #             z = int(self.scenarios/self.design[-1])                 # Number of parent nodes
-    #             for j in range(self.scenarios):                         # Add parent node ids to child nodes
-    #                 D[i][j].parent_id = self.scenarios + j % z          # Parent node ID
-    #                 D[i][j].level = self.levels - i                     # Level
-
-    #         else:    # If not first level
-    #             D.append([])
-    #             for j in range(self.levels**(self.levels-i)):           # Add child nodes to parent node
-    #                 node_id = node + j                                  # Node ID
-    #                 data = self.x[j]                                    # Data
-    #                 D[i].append(Node(node_id, data, self.levels - i))   # Add node to list
-
-    #             # Add parent node ids to child nodes
-    #             z = int(self.levels**(self.levels-i-1))                 # Number of parent nodes
-    #             for j in range(self.levels**(self.levels-i)):           # Add parent node ids to child nodes
-    #                 D[i][j].parent_id = node + self.levels**(self.levels-i) + j % z     # Parent node ID
-    #                 D[i][j].level = self.levels - i                     # Level
-
-    #                 # Add child node ids to parent node
-    #                 parent_node = D[i][j]                               # Parent node
-    #                 parent_node.child_ids = []                          # Child node ids
-    #                 for child_node in D[i-1]:                           # Iterate through child nodes
-    #                     if child_node.parent_id == parent_node.node_id:     # If child node is a child of parent node
-    #                         parent_node.child_ids.append(child_node.node_id)    # Add child node id to parent node
-    #         node += self.levels**(self.levels-i)                        # Update node id
-    #     D.append([])
-    #     node_id = node
-    #     data = self.x
-    #     level = 0
-    #     D[-1].append(Node(node_id, data, level))                        # Add root node
-    #     D = [elem for sublist in D for elem in sublist]                 # Flatten list
-    #     self.data = D                                                   # Tree data
